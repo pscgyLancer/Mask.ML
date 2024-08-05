0575de76a7c2393d440dabab52d7fe72272eb2d9
Lance <lance.wu@didatravel.com>
2024-08-05 17:26:23 +0800

    update net6

---
+++ Mask.ML.WebApi/Controllers/DiscernController.cs
@@ -0,0 +1,72 @@
+﻿using Mask_MLML.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Mask.ML.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DiscernController : ControllerBase
+    {
+        /// <summary>
+        /// 上传文件：口罩验证
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="fileName"></param>
+        [HttpPost("upload")]
+        public IActionResult UploadFile(IFormFile file)
+        {
+            //申明返回的结果
+            string result = "";
+
+            //做随机数，用到文件夹名字上，防重名
+            Random random = new Random();
+            string r = "";
+            int i;
+            for (i = 1; i < 11; i++)
+            {
+                r += random.Next(0, 9).ToString();
+            }
+            //文件路径
+            string FilePath = AppDomain.CurrentDomain.BaseDirectory + "/TempFiles/";
+            string name = file.FileName;
+            string FileName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + r;
+            //获取文件类型
+            string type = System.IO.Path.GetExtension(name);
+            DirectoryInfo di = new DirectoryInfo(FilePath);
+            if (!di.Exists)
+            {
+                di.Create();
+            }
+            //文件保存的路径
+            var filefullname = FilePath + FileName + type;
+            using (FileStream fs = System.IO.File.Create(filefullname))
+            {
+                // 复制文件
+                file.CopyTo(fs);
+                // 清空缓冲区数据
+                fs.Flush();
+                fs.Close();
+                fs.Dispose();
+            }
+            //成功提示赋值到返回结果中
+            //result = "文件上传成功";
+
+            // 创建样例数据的单个实例对模型输入数据集的第一行
+            ModelInput sampleData = new ModelInput()
+            {
+                ImageSource = filefullname,
+            };
+            // 获取预测结果
+            var predictionResult = ConsumeModel.Predict(sampleData);
+            //System.IO.File.Delete(filefullname);
+            return Ok(predictionResult.Prediction);
+
+        }
+
+    }
+}
+
---
+++ Mask.ML.WebApi/Startup.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mask.ML.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Mask.ML",
+                    Version = "v1",
+                    Description = "框架说明文档"
+                });
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mask.ML v1");
+                c.RoutePrefix = string.Empty;//如果设置根目录为swagger,将此值置空 
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
